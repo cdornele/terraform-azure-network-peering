07643affb23fccd2514ea263cb48a58e1003edb5
Clairo Dorneles <96629866+cdornele@users.noreply.github.com>
2025-03-02 12:05:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
 resource "azurerm_virtual_network_peering" "source-to-destination" {
   provider = azurerm.src
   name                         = format("PE-%s-to-%s", var.vpeer_src_prefix, var.vpeer_dest_prefix)
-  resource_group_name          = var.dest_resource_group_name
+  resource_group_name          = var.src_resource_group_name
   virtual_network_name         = var.src_name
   remote_virtual_network_id    = var.dest_virtual_network_id
   allow_virtual_network_access = var.src_virtual_network_access_allowed
@@ -17,7 +17,7 @@ resource "azurerm_virtual_network_peering" "source-to-destination" {
 resource "azurerm_virtual_network_peering" "destination-to-source" {
   provider = azurerm.dst
   name                         = format("PE-%s-to-%s", var.vpeer_dest_prefix, var.vpeer_src_prefix)
-  resource_group_name          = var.src_resource_group_name
+  resource_group_name          = var.dest_resource_group_name
   virtual_network_name         = var.dest_name
   remote_virtual_network_id    = var.src_virtual_network_id
   allow_virtual_network_access = var.dest_virtual_network_access_allowed
